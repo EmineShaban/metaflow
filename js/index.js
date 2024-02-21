96c402d052f6f356a35dc9f395a2818136268bab
Emine Shaban <emineshaban@icloud.com>
2024-02-21 16:03:47 +0200

    .

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let click = document.getElementsByClassName("ingr-title");
+
+let text = document.getElementsByClassName("ingr-text");
+
+const onClick = (e) => {
+  console.log(e.target.parentElement.children[1]);
+  console.log(e.target)
+  if (e.target.parentElement.parentElement.children[1].style.display == "block") {
+    e.target.parentElement.parentElement.children[1].style.display = "none";
+    e.target.parentElement.children[1].textContent = "+";
+  } else {
+    e.target.parentElement.parentElement.children[1].style.display = "block";
+    e.target.parentElement.children[1].textContent = "-";
+  }
+  console.log(text);
+};
+for (let index = 0; index < click.length; index++) {
+  click[index].addEventListener("click", onClick);
+}
+
+// for (let i = 0; i < text.length; i++) {
+//     console.log(text[i])
+
+// }

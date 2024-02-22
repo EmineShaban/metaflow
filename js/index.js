d50c37d029dc2649cd52fda95636456c495a4f80
Emine Shaban <emineshaban@icloud.com>
2024-02-22 16:10:39 +0200

    .

--- js/index.js
+++ js/index.js
@@ -4,16 +4,15 @@ let text = document.getElementsByClassName("ingr-text");
 
 const onClick = (e) => {
   console.log(e.target.parentElement.children[1]);
-  console.log(e.target)
-  if ( e.target.parentElement.children[1].textContent == "-") {
+  console.log(e.target);
+  if (e.target.parentElement.children[1].textContent == "-") {
     e.target.parentElement.parentElement.children[1].style.display = "none";
     e.target.parentElement.children[1].textContent = "+";
   } else {
-    if(e.target.parentElement.children[1].textContent == "+"){
-        e.target.parentElement.parentElement.children[1].style.display = "block";
-        e.target.parentElement.children[1].textContent = "-";
+    if (e.target.parentElement.children[1].textContent == "+") {
+      e.target.parentElement.parentElement.children[1].style.display = "block";
+      e.target.parentElement.children[1].textContent = "-";
     }
-   
   }
   console.log(text);
 };
@@ -21,7 +20,49 @@ for (let index = 0; index < click.length; index++) {
   click[index].addEventListener("click", onClick);
 }
 
-// for (let i = 0; i < text.length; i++) {
-//     console.log(text[i])
+//pagination
 
-// }
+let digit = document.getElementsByClassName("digit");
+const onChange = (e) => {
+  if (e.target.textContent == "1") {
+    console.log(e.target.parentElement.children);
+    for (let i = 0; i < e.target.parentElement.parentElement.children.length; i++) {
+        if(i>=3 && i< e.target.parentElement.parentElement.children.length - 1){
+            e.target.parentElement.parentElement.children[i].style.display = "none";
+        }
+    }
+    e.target.parentElement.parentElement.children[3].style.display = "block";
+    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+      e.target.parentElement.children[i].classList = "digit";
+    }
+    e.target.classList = "digit active";
+  } else if (e.target.textContent == "2") {
+    console.log(e.target.parentElement.children);
+    for (let i = 0; i < e.target.parentElement.parentElement.children.length; i++) {
+        if(i>=3 && i< e.target.parentElement.parentElement.children.length - 1){
+            e.target.parentElement.parentElement.children[i].style.display = "none";
+        }
+    }
+    e.target.parentElement.parentElement.children[4].style.display = "block";
+    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+      e.target.parentElement.children[i].classList = "digit";
+    }
+    e.target.classList = "digit active";
+  } else if (e.target.textContent == "3") {
+    console.log(e.target.parentElement.children);
+    for (let i = 0; i < e.target.parentElement.parentElement.children.length; i++) {
+        if(i>=3 && i< e.target.parentElement.parentElement.children.length - 1){
+            e.target.parentElement.parentElement.children[i].style.display = "none";
+        }
+    }
+    e.target.parentElement.parentElement.children[5].style.display = "block";
+    for (let i = 0; i < e.target.parentElement.children.length; i++) {
+      e.target.parentElement.children[i].classList = "digit";
+    }
+    e.target.classList = "digit active";
+  }
+};
+
+for (let index = 0; index < digit.length; index++) {
+  digit[index].addEventListener("click", onChange);
+}
